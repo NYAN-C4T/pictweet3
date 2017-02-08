d556de8ab8af0598df376c71f7523320717daf80
NYAN-C4T <tesseract13th@outlook.com>
2017-02-08 20:31:11 +0900

    reshape indent and confirm password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a nickname" do
-     user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-     user.valid?
-     expect(user.errors[:nickname]).to include("can't be blank")
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "is invalid without a password" do
+      user = User.new(nickname: "mockmock", email: "kkk@gmail.com", password: "", password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
     end
   end
 end
