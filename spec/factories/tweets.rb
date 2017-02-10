3a6eaa48fe88ff231bda6f8198f810c367da0d1b
NYAN-C4T <tesseract13th@outlook.com>
2017-02-10 10:59:20 +0900

    full test

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :tweet do
-    text "hello!"
+    text "foo"
     image "hoge.png"
     user_id 1
+    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 end
